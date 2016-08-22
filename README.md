f9fbb9542121f0680926f8f5181a5f7fbfffd2b6
Zach Hedgepeth <Zhedgepeth1124@gmail.com>
2016-08-21 21:01:08 -0400

    eventually push the hope to be last track survey question about removing images once new survey submitted

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Track Survey_
+
+#### _A survey to identify which programming language track best suits the user, 8/19/2016_
+
+#### By _**Zachary Hedgepeth**_
+
+## Description
+
+_A survey to identify which programming language track best suits the user. It takes the input of a variety of questions, and depending upon the combination of answers, the user is told which programming track best suits their desires._
+
+## Setup/Installation Requirements
+
+* _Open Github_
+* _Go to the user name: Zhedgepeth_
+* _Open the repository 'track-survey'_
+* _Copy the url in the upper right hand_
+* _Open the terminal and git clone "url copied"_
+* _Open in atom or which interface you desire.
+
+
+## Known Bugs
+
+_There are no known bugs at this time_
+
+## Support and contact details
+
+_If you have any questions contact me at: Zhedgepeth1124@gmail.com_
+
+## Technologies Used
+
+_I used javascript and if/else statements._
+
+### License
+
+*Epicodus?*
+
+Copyright (c) 2016 **_Zachary Hedgepeth_**
---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $("form#info").submit(function(event) {
+    event.preventDefault();
+
+    var experience = $("input:radio[name=experience]:checked").val();
+    var industry = $("input:radio[name=industry]:checked").val();
+    var interaction = $("input:radio[name=interaction]:checked").val();
+    var companyType = $("input:radio[name=companyType]:checked").val();
+    var application = $("input:radio[name=application]:checked").val();
+
+    if (experience === 'a-lot') {
+    //experience level
+      if (industry === 'web-dev') {
+      //industry
+        if (interaction === 'moderate') {
+        //user interaction
+          if (companyType === 'enterprise') {
+          //enterprise
+            if (application === 'android') {
+            //app type
+            $('#java').show();
+            } else if (application === 'web') {
+              $('#php').show();
+            } else {
+              $('#csharp').show();
+            }
+          } else {
+            // experience: alot, industry: web dev, interaction: moderate, companyType: startup
+            if (application === 'web') {
+              $('#ruby').show();
+            } else if (application === 'windows') {
+              $('#csharp').show();
+            } else {
+              $('#java').show();
+            }
+          }
+
+        } else {
+          //alot, web dev, heavy
+            if (companyType === 'enterprise' || companyType === 'startup') {
+            // alot, webdev, heavy, enterprise or startup'
+              if (application === 'android') {
+              $('#java').show();
+            } else if (application === 'windows') {
+              $('#csharp').show();
+            } else {
+              $('#ruby').show();
+            }
+          }
+        }
+
+      } else {
+        // alot, software
+          if (interaction === 'moderate' || interaction === 'heavy') {
+          // alot, software, moderate or heavy
+            if (companyType === 'enterprise' || companyType === 'startup') {
+            // alot, software, moderate or heavy, and enterprise
+              if (application === 'windows') {
+              $('#csharp').show();
+            } else {
+              $('#php').show();
+            }
+            }
+          }
+        }
+
+    } else {
+        // no experience,
+        if (industry === 'web-dev' || industry === 'software') {
+        //no experience and web-dev or software engineering
+          if (interaction === 'heavy') {
+          //no experience and web-dev or software engineering, and heavy interaction
+            if (companyType === 'enterprise' || companyType === 'startup') {
+            //no experience/web-dev or software engineering/heavy interaction/ enterprise or startup
+              if (application === 'windows' || application ==='android') {
+              $('#csharp').show();
+            } else {
+              $('#ruby').show();
+            }
+            }
+          } else {
+             //no experience and web-dev or software engineering, moderate interaction
+              if (companyType === 'enterprise' || companyType === 'startup') {
+              // no experience, web-dev/software, moderate, enterprise/startup
+                if (application === 'windows' || application ==='android') {
+                $('#php').show();
+              } else {
+                $('#ruby').show();
+              }
+              }
+            }
+          }
+        }
+
+  });
+});
